7f2fbe027875aab6371ea355c2bc2669ac59fffb
Semyon Koltsov <semyonkoltsov98@gmail.com>
2025-10-12 21:04:08 +0300

    user repo interface updated

--- user/repository.go
+++ user/repository.go
@@ -1,14 +1,17 @@
 package user
 
+import "time"
+
 type UserId int64
 
 type User struct {
-	ID              UserId `json:"id"`
-	Name            string `json:"name"`
-	ReddyID         string `json:"reddy_id"`
-	LastDayNotified string `json:"last_day_notified"`
+	ID               UserId    `json:"id"`
+	Name             string    `json:"name"`
+	LastTimeNotified time.Time `json:"last_time_notified"`
 }
 
 type IUserRepository interface {
 	GetAll() ([]User, error)
+	GetById(id UserId) (User, error)
+	Create(User) (User, error)
 }
