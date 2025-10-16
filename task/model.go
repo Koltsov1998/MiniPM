e82c8ac22f28bc323034ed9ac52c32ff70b133a4
Semyon Koltsov <semyonkoltsov98@gmail.com>
2025-10-16 22:14:40 +0300

    Changed repository interface

---
+++ task/model.go
@@ -0,0 +1,5 @@
+package task
+
+type Task struct {
+	Title string
+}
--- task/repository.go
+++ task/repository.go
@@ -4,12 +4,8 @@ import (
 	"github.com/Koltsov1998/MiniPM/user"
 )
 
-type Task struct {
-	Title string
-}
-
 type ITaskRepository interface {
-	GetAllInProgress(userIds []user.UserId) (map[user.UserId][]Task, error)
-	GetAllInProgressForUser(userId user.UserId) ([]Task, error)
-	WriteTaskReport(userId user.UserId, reportMessage string) error
+	GetAllInProgress(users []user.User) (map[user.UserId][]Task, error)
+	GetAllInProgressForUser(user user.User) ([]Task, error)
+	WriteTaskReport(user user.User, reportMessage string) error
 }
